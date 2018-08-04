1c8a1232eba17f3afe4ec07aee1deee788479f3f
thanh <lookat018@gmail.com>
2018-08-04 17:55:42 +0700

    update 84

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var todoController = require('./controllers/todoController');
-
+var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
 // localhost:3000/assets/style.css
+
+app.use(session({
+    secret: 'Thisismytestkey',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 todoController(app);
 
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,49 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+// mongoose.connect('mongodb://test:test1234@ds259001.mlab.com:59001/little2');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/todo', { useNewUrlParser: true });
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'coding'}];
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+// var itemOne = Todo({item: 'buy flower'}).save(function(err){
+//     if (err) throw err
+//     console.log('save item');
+// });
+
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'coding'}];
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
     app.get('/todo', function(req, res) {
-        res.render('todo', {todos: data});
+        Todo.find({}, function(err, data) {
+            if(err) throw err;
+            res.render('todo', {todos: data});
+        })
     });
 
     app.post('/todo', urlencodedParser, function(req, res) {
+        var newTodo = Todo(req.body).save(function(err, data) {
+            if(err) throw err;
+            res.json(data);
+        });
         console.log(req.body);
-        data.push(req.body);
-        res.json(data);
+        // data.push(req.body);
+        // res.json(data);
     });
 
     app.delete('/todo/:item', function(req, res) {
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if(err) throw err;
+            res.json(data);
         });
-        res.json(data);
     });
 };
+
+
 
